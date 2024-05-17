e5f97f4e4898b8fec3f50aefa5bac45bcb3bde49
HIronGH_PC <xxiron1ghostxx@gmail.com>
2024-05-17 19:27:50 +0300

    Main .py File

--- Health_Test.py
+++ Health_Test.py
@@ -1,9 +1,49 @@
-#das
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont  # перевірка типів значень, що вводяться
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout, QGridLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
 
-a,b = 3,6
+#from instr import *
+#from second_win import *
 
-for num in range (b):
-    print("NO")
+class MainWin(QWidget):
+    def __init__(self):
+        super.__init__()
+        self.set_apper()
+        self.initUI()
+        self.connects()
+        self.show()
 
-for num in range (a):
-    print("YES!")
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.layout = QVBoxLayout()
+
+        self.layout.addWidget(self.hello_text, alligment=Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alligment=Qt.AlignLeft)
+        self.layout.addWidget(self.btn_next, alignment=Qt.AlignLeft)
+
+        self.setLayout(self.layout)
+
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+    def set_apper(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+def main():
+    app = QApplication([])
+    mw = MainWin()
+    app.exec_()
+
+main()
